d5b7c89a5795e7be22455c276e663aebefd7fd3b
sunbaixin <283122529@qq.com>
2021-05-18 10:56:45 +0800

    add token contract record

--- controllers/TokenControllers.go
+++ controllers/TokenControllers.go
@@ -13,6 +13,9 @@ type TokenListController struct {
 	BaseController
 }
 
+
+
+
 type TokenListStruct []struct {
 	Name      string `json:"name"`
 	Image     string `json:"image"`
@@ -27,27 +30,38 @@ type ApiContractBalanceController struct {
 func (c *TokenListController) Post() {
 
 	address := c.GetString("address")
-
-	source, _ := ioutil.ReadFile("conf/tokens.json")
-	var tokensResult []map[string]string
-	err := json.Unmarshal(source, &tokensResult)
-	if err != nil {
-		fmt.Println("tokens json err: ", err)
-	}
-
-	for i := 0; i < len(tokensResult); i++ {
-		result, _, _ := models.TokenBalanceFunction(address, tokensResult[i]["ct_address"], tokensResult[i]["type"], "balance", []string{address})
-		switch result.(type) { //这里是通过i.(type)来判断是什么类型  下面的case分支匹配到了 则执行相关的分支
-		case map[string]interface{}:
-			data := result.(map[string]interface{})
-			balances := data["Some"].([]interface{})
-			balanceFloat, _ := balances[0].(json.Number).Float64()
-			balanceFloatFormat := strconv.FormatFloat(balanceFloat/1000000000000000000, 'f', 5, 64)
-			tokensResult[i]["count"] = balanceFloatFormat
+	t := c.GetString("type")
+	if t == ""{
+		source, _ := ioutil.ReadFile("conf/tokens.json")
+		var tokensResult []map[string]string
+		err := json.Unmarshal(source, &tokensResult)
+		if err != nil {
+			fmt.Println("tokens json err: ", err)
 		}
 
+		for i := 0; i < len(tokensResult); i++ {
+			result, _, _ := models.TokenBalanceFunction(address, tokensResult[i]["ct_address"], tokensResult[i]["type"], "balance", []string{address})
+			switch result.(type) { //这里是通过i.(type)来判断是什么类型  下面的case分支匹配到了 则执行相关的分支
+			case map[string]interface{}:
+				data := result.(map[string]interface{})
+				balances := data["Some"].([]interface{})
+				balanceFloat, _ := balances[0].(json.Number).Float64()
+				balanceFloatFormat := strconv.FormatFloat(balanceFloat/1000000000000000000, 'f', 5, 64)
+				tokensResult[i]["count"] = balanceFloatFormat
+			}
+
+		}
+		c.SuccessJson(tokensResult)
+	}else{
+		source, _ := ioutil.ReadFile("conf/tokens.json")
+		var tokensResult []map[string]string
+		err := json.Unmarshal(source, &tokensResult)
+		if err != nil {
+			fmt.Println("tokens json err: ", err)
+		}
+		c.SuccessJson(tokensResult)
 	}
-	c.SuccessJson(tokensResult)
+
 }
 
 
